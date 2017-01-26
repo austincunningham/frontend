4a24b51341a84c7bcf12eb38804a007b0dabeadc
austincunningham <austincunningham@hotmail.com>
2017-01-26 20:23:40 +0000

    update now works

--- app/js/controllers/FindController.js
+++ app/js/controllers/FindController.js
@@ -5,28 +5,37 @@ app.controller('FindController', function ($scope, $http) {
     /*$scope.firstName = 'First Name';
     $scope.lastName = 'Last Name';
     $scope.email = 'Email';*/
-    var id = $scope._id;
+
 
     $scope.FindUser = function () {
-        $http.get('http://localhost:8000/users/'+ id)
-            .success(function(data) {
+        var id = $scope._id;
+        $http.get('http://localhost:8000/users/' + id)
+            .success(function (data) {
                 $scope.firstName = data.name.first;
                 $scope.lastName = data.name.last;
                 $scope.email = data.email;
                 return data;
-            })
-            .error(function(err){
+              })
+            .error(function (err) {
                 return err;
+              });
+
+      };
+
+    $scope.UpdateData = function () {
+        var id = $scope._id;
+        var data = {
+          name: {
+            first: $scope.firstName,
+            last: $scope.lastName,
+          },
+          email: $scope.email,
+        };
+
+        $http.put('http://localhost:8000/users/' + id, data)
+            .success(function (data) {
+
             });
+      };
 
-    };
-
-    $scope.UpdateData = function() {
-
-        $http.put('http://localhost:8000/users/'+id, data )
-            .success(function(data) {
-
-            });
-    }
-
-});
+  });
